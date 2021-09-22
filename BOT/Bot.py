f17c7f4ec198e7f1e39d394367f3ccccbb560c80
ViljamiLampila <viljamilampila@gmail.com>
2021-09-22 22:30:08 +0300

    deleted unnessecary files and patched the code

--- BOT/Bot.py
+++ BOT/Bot.py
@@ -10,6 +10,7 @@ import json
 with open("config.json", "r") as config:
     data = json.load(config)
     token = data["token"]
+    password = data["password"]
     prefix = data["prefix"]
 
 
@@ -24,10 +25,13 @@ intents.members = True
 
 bot = commands.Bot(prefix, intents = intents)
 
+
 # Load cogs
 initial_extensions = [
     "Cogs.help",
-    "Cogs.ping"
+    "Cogs.ping",
+    "Cogs.maincog",
+    "Cogs.hello"
 ]
 
 print(initial_extensions)
@@ -35,7 +39,7 @@ print(initial_extensions)
 if __name__ == '__main__':
     for extension in initial_extensions:
         try:
-            bot.load_extension(extension)
+            bot.load_extension(extension)           
         except Exception as e:
             print(f"Failed to load extension {extension}")
 
@@ -44,6 +48,9 @@ async def on_ready():
     print(f"We have logged in as {bot.user}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
     print(discord.__version__)
+    
+
+    
 
 bot.run(token)
 

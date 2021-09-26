6d814a1843a759e12939ef8138941f39e071eb4c
ViljamiLampila <viljamilampila@gmail.com>
2021-09-26 19:42:05 +0300

    client works now with cogs, initiate self as client and use commands.bot

--- BOT/Cogs/maincog.py
+++ BOT/Cogs/maincog.py
@@ -1,4 +1,23 @@
 import discord
+import random
+from discord import client
 from discord.ext import commands
 
 
+
+class test(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.bot = client
+
+    @commands.command(name="ping")
+    async def ping(self, ctx: commands.Context):
+        """Get the bot's current websocket latency."""
+        await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms")
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+
+
+def setup(client: commands.Bot):
+    client.add_cog(test(client))
--- BOT/selfbot.py
+++ BOT/selfbot.py
@@ -1,62 +1,11 @@
-import os
+from os import getenv
 import json
 import discord
 from discord.ext import commands
 
 
+client = commands.Bot(command_prefix="!")
 
-with open("config.json", "r") as config:
-    data = json.load(config)
-    token = data["token"]
-    prefix = data["prefix"]
+client.load_extension("Cogs.maincog")
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('logged on as ', self.user)
-
-    async def on_message(self, message):
-
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
-        
-
-    
-
-class Greetings(commands.Cog):
-    def __init__(self, bot:commands.Bot):
-        self.bot = bot
-        self._last_member = None
-        
-# Intents
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(prefix, intents = intents)
-
-# Load cogs
-initial_extensions = [
-    "Cogs.help",
-    "Cogs.ping",
-    "Cogs.maincog",
-    "Cogs.hello"
-]
- 
-
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        try:
-            bot.load_extension(extension)           
-        except Exception as e:
-            print(f"Failed to load extension {extension}")
-
-print(initial_extensions)
-
-client = MyClient()
-client.run(token)
+client.run("ODg5OTY5NDY0MDU2MTAyOTQy.YUzneQ.Jjp4D-SpgfGd9MYzfrOG1TfM2rk")

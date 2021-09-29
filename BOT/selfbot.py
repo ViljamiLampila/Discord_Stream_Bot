dc8b601907adfa5797961584286c8c8e7f9b6726
ViljamiLampila <viljami.lampila@gmail.com>
2021-09-29 12:53:15 -0400

    added comments to code and removed unnecessary code

--- BOT/selfbot.py
+++ BOT/selfbot.py
@@ -15,20 +15,19 @@ class main(discord.Client):
             return
 
 
-        else:
+        if "!play" in message.content:
             userText = message.content
-            var1, var2 = userText.split(" ")
+            var1, var2 = userText.split(" ") #checking if the url provided is valid
             if ((var1 == "!play") and (validators.url(var2) == True)):
 
                    
                 sp = subprocess.Popen(["vlc", var2])               
                 poll = sp.poll()
-
+                #if poll returns none vlc is active then add selected song to que
                 if poll is None:
                     subprocess.Popen(["vlc --started-from-file --playlist-enqueue", var2])
-
-                else:           
-                    sp()
+                #if not active start subprocess and check for errors
+                else:                            
                     raiseExceptions()
 
 

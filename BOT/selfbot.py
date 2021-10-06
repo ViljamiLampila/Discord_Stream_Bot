11fed1d9a666b096544e34e03d7abf07adbd4f7d
KeremKavlak <keremakavlak@gmail.com>
2021-10-06 10:18:10 +0100

    idk

--- BOT/selfbot.py
+++ BOT/selfbot.py
@@ -18,7 +18,9 @@ class main(discord.Client):
         if "!play" in message.content:
             userText = message.content
             var1, var2 = userText.split(" ") #checking if the url provided is valid
-            nodePuppet()
+            nodePuppet() #runs BrowserPuppet.js
+            subprocess.Popen(["ffmpeg", "-f", "x11grab", "-r", "24", "-s", "1920x1080", "-i", ":0.0+0", "-vcodec", "rawvideo", "-pix_fmt", "yuv420p", "-threads", "0", "-f", "v4l2", "/dev/video0"]) #runs virtual camera feed
+            #one for audio too would be nice
             if ((var1 == "!play") and (validators.url(var2) == True)):
 
                    
@@ -26,7 +28,7 @@ class main(discord.Client):
                 poll = sp.poll()
                 #if poll returns none vlc is active then add selected song to que
                 if poll is None:
-                    subprocess.Popen(["vlc --started-from-file --playlist-enqueue", var2])
+                    subprocess.Popen(["vlc -f --started-from-file --playlist-enqueue", var2])
                 #if not active start subprocess and check for errors
                 else:                            
                     raiseExceptions()
